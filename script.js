e151b10a9bb7562a47c77b89d681878cf1a47947
dextermorgan079 <dextermorgan079@gmail.com>
2026-04-28 12:35:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document.querySelectorAll('.faq-item').forEach(item => {
     btn.addEventListener('click', () => item.classList.toggle('active'));
   }
 });
+const elements = document.querySelectorAll('.fade-up');
+
+  window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+      const top = el.getBoundingClientRect().top;
+      if (top < window.innerHeight - 50) {
+        el.classList.add('show');
+      }
+    });
+  });
+
+});
